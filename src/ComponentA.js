11de9957a746d32d4e19eef28611ff7db7dd6a1c
Manas Rajdeep <manasrajdeep@outlook.com>
2026-07-09 09:38:59 +0530

    .

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,28 +1,36 @@
 import React from "react";
 
 class ComponentA extends React.Component {
+
+
     constructor(){
         super();
 
         this.state ={
-            name:"ComponentA"
+            name:"ComponentA "
         }
 
         console.log("ComponentA constructor")
 
     }
 
+
+
     static getDerivedStateFromProps(){
         console.log("ComponentA getDerivedStateFromProps")
         return null;
     }
 
+
     componentDidMount(){
         console.log("ComponentA  componentDidMount")
     }
 
+
+    
     render(){
          console.log("ComponentA render")
+         
         return(
             <h1> ComponentA </h1>
         );
